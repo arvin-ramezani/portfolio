41d062ac67486bf3ae151651adbb6b8475c1337a
A Plus Web <a.plus.rmz@gmail.com>
2023-06-12 17:48:42 +0330

    Work on Skills component, add dynamic data and animation

--- components/skills/skills.tsx
+++ components/skills/skills.tsx
@@ -1,7 +1,7 @@
 import React, { useRef } from 'react';
 import { useTranslation } from 'next-i18next';
 import Image from 'next/image';
-import { Variants, useInView } from 'framer-motion';
+import { AnimatePresence, Variants, useInView } from 'framer-motion';
 
 import {
   SkillText,
@@ -11,6 +11,24 @@ import {
   SkillsTitle,
 } from '@/styles/components/skills.styled';
 import { aboutItemsVariants } from './skills.variants';
+import { SKILLS_LIST } from '@/utils/data/skills.data';
+
+const skillsContainerVariants: Variants = {
+  initial: { opacity: 0 },
+  animate: {
+    opacity: 1,
+    transition: { when: 'beforeChildren', staggerChildren: 0.2 },
+  },
+};
+
+const skillsItemsVariants: Variants = {
+  initial: { y: -40, opacity: 0 },
+  animate: (index) => ({
+    y: 0,
+    opacity: 1,
+    transition: { duration: 0.4, delay: index * 0.2 },
+  }),
+};
 
 const Skills = () => {
   const { t: translator } = useTranslation();
@@ -18,129 +36,39 @@ const Skills = () => {
   const isSkillsInView = useInView(skillsRef);
 
   return (
-    <StyledSkills
-      variants={aboutItemsVariants}
-      ref={skillsRef}
-      custom={'right'}
-      initial="initial"
-      animate={isSkillsInView ? 'animate' : 'exit'}
-      exit={'exit'}
-    >
+    <StyledSkills>
       <SkillsTitle>{translator('home:skills_title')}</SkillsTitle>
-      <SkillsItemBlock>
-        <SkillsItem>
-          <Image
-            src="./images/icons/html.svg"
-            width={45}
-            height={40}
-            alt="html"
-          />
 
-          <SkillText>{'HTML'}</SkillText>
-        </SkillsItem>
-        <SkillsItem>
-          <Image
-            src="./images/icons/css.svg"
-            width={45}
-            height={40}
-            alt="css"
-          />
+      <AnimatePresence>
+        <SkillsItemBlock
+          ref={skillsRef}
+          variants={skillsContainerVariants}
+          initial={'initial'}
+          animate={isSkillsInView ? 'animate' : 'initial'}
+          exit={'initial'}
+          key="skillsContainer"
+        >
+          {SKILLS_LIST.map((skill, index) => (
+            <SkillsItem
+              key={skill.name}
+              variants={skillsItemsVariants}
+              initial={'initial'}
+              animate={isSkillsInView ? 'animate' : 'initial'}
+              exit={'initial'}
+              custom={index}
+            >
+              <Image
+                src={skill.image}
+                width={45}
+                height={40}
+                alt={skill.name}
+              />
 
-          <SkillText>{'CSS'}</SkillText>
-        </SkillsItem>
-        <SkillsItem>
-          <Image
-            src="./images/icons/javascript.svg"
-            width={45}
-            height={40}
-            alt="javascript"
-          />
-
-          <SkillText>{'JavaScript'}</SkillText>
-        </SkillsItem>
-        <SkillsItem>
-          <Image
-            src="./images/icons/nodejs.svg"
-            width={45}
-            height={40}
-            alt="nodejs"
-          />
-
-          <SkillText>{translator('home:skill_name_nodejs')}</SkillText>
-        </SkillsItem>
-        <SkillsItem>
-          <Image
-            src="./images/icons/reactjs.svg"
-            width={45}
-            height={40}
-            alt="nodejs"
-          />
-
-          <SkillText>{'ReactJS'}</SkillText>
-        </SkillsItem>
-        <SkillsItem>
-          <Image
-            src="./images/icons/nextjs.svg"
-            width={45}
-            height={40}
-            alt="nextjs"
-          />
-          <SkillText>{translator('home:skill_name_nextjs')}</SkillText>
-        </SkillsItem>
-        <SkillsItem>
-          <Image
-            src="./images/icons/redux.svg"
-            width={45}
-            height={40}
-            alt="redux"
-          />
-          <SkillText>{'Redux'}</SkillText>
-        </SkillsItem>
-        <SkillsItem>
-          <Image
-            src="./images/icons/styled-components.svg"
-            width={120}
-            height={40}
-            alt="styled-components"
-          />
-        </SkillsItem>
-        <SkillsItem>
-          <Image
-            src="./images/icons/framer-motion.svg"
-            width={45}
-            height={40}
-            alt="framer-motion"
-          />
-          <SkillText style={{ fontSize: '.8rem' }}>{'Framer Motion'}</SkillText>
-        </SkillsItem>
-        <SkillsItem>
-          <Image
-            src="./images/icons/nestjs.svg"
-            width={45}
-            height={40}
-            alt="nextjs"
-          />
-          <SkillText>{'NestJS'}</SkillText>
-        </SkillsItem>
-        <SkillsItem>
-          <Image
-            src="./images/icons/typescript.svg"
-            width={45}
-            height={40}
-            alt="nextjs"
-          />
-          <SkillText>{'TypeScript'}</SkillText>
-        </SkillsItem>
-        <SkillsItem>
-          <Image
-            src="./images/icons/mongoDB.svg"
-            width={45}
-            height={40}
-            alt="mongodb"
-          />
-          <SkillText>{'MongoDB'}</SkillText>
-        </SkillsItem>
-      </SkillsItemBlock>
+              <SkillText>{skill.name}</SkillText>
+            </SkillsItem>
+          ))}
+        </SkillsItemBlock>
+      </AnimatePresence>
     </StyledSkills>
   );
 };
---
+++ utils/data/skills.data.ts
@@ -0,0 +1,52 @@
+import { ISkill } from '../types/skill.types';
+
+export const SKILLS_LIST: ISkill[] = [
+  {
+    image: '/images/icons/html.svg',
+    name: 'HTML',
+  },
+  {
+    image: '/images/icons/css.svg',
+    name: 'CSS',
+  },
+  {
+    image: '/images/icons/javascript.svg',
+    name: 'JavaScript',
+  },
+  {
+    image: '/images/icons/nodejs.svg',
+    name: 'NodeJS',
+  },
+  {
+    image: '/images/icons/reactjs.svg',
+    name: 'ReactJS',
+  },
+  {
+    image: '/images/icons/nextjs.svg',
+    name: 'NextJS',
+  },
+  {
+    image: '/images/icons/redux.svg',
+    name: 'Redux',
+  },
+  {
+    image: '/images/icons/styled-components.svg',
+    name: 'Styled-Components',
+  },
+  {
+    image: '/images/icons/framer-motion.svg',
+    name: 'Freamer Motion',
+  },
+  {
+    image: '/images/icons/nestjs.svg',
+    name: 'NestJS',
+  },
+  {
+    image: '/images/icons/typescript.svg',
+    name: 'TypeScript',
+  },
+  {
+    image: '/images/icons/mongodb.svg',
+    name: 'MongoDB',
+  },
+];
