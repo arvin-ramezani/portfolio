c4d6904f416c26b4b00d5485b5d60c508ad33a31
A Plus Web <a.plus.rmz@gmail.com>
2023-07-16 19:53:37 +0330

    Chang styles of hero-section.tsx heading text color

--- components/hero-section/hero-section.tsx
+++ components/hero-section/hero-section.tsx
@@ -1,67 +1,30 @@
 import Image from 'next/image';
 import React, { useState, useEffect } from 'react';
-import { isMobile } from 'react-device-detect';
-import { useTranslation } from 'next-i18next';
 import { useRouter } from 'next/router';
 
 import {
   HeroImageContainer,
-  HeroTextContainer,
   StyledHeroSection,
 } from '@/styles/components/hero-section.styled';
 import { Container } from '@/styles/global.styled';
-import Button from '../ui/button/button';
-import { theme } from '@/styles/themes/theme.styled';
-import { heroImageVariants, heroTextVariants } from './hero-section.variants';
+import { heroImageVariants } from './hero-section.variants';
+import HeroSectionText from './hero-section-text/hero-section-text';
 
 function HeroSection() {
   const router = useRouter();
-  const { t: translator } = useTranslation();
   const [pageDir, setPageDir] = useState<'rtl' | 'ltr'>(
     router.locale === 'fa' ? 'rtl' : 'ltr'
   );
 
-  const onCall = () => {
-    if (isMobile) {
-      window.open('tel:+989361599686');
-
-      return;
-    }
-
-    router.push('/#contact');
-  };
-
   useEffect(() => {
+    console.log(router.locale, 'locale');
     setPageDir(router.locale === 'fa' ? 'rtl' : 'ltr');
   }, [router.locale]);
 
   return (
     <StyledHeroSection>
       <Container>
-        <HeroTextContainer
-          variants={heroTextVariants}
-          initial="hidden"
-          animate="visible"
-          custom={pageDir}
-        >
-          <h1>{translator('home:hero_section_heading')}</h1>
-
-          <h2>{translator('home:hero_section_sub_heading_1')}</h2>
-          <p>{translator('home:hero_section_sub_heading_2')}</p>
-
-          {pageDir === 'ltr' && (
-            <p>{translator('home:hero_section_sub_heading_3')}</p>
-          )}
-
-          <Button
-            text={`${translator('common:call_me_btn')}${
-              pageDir === 'rtl' ? 'بگیرید' : ''
-            }`}
-            onClick={onCall}
-            color={theme.colors.primary}
-            textColor={theme.colors.black}
-          />
-        </HeroTextContainer>
+        <HeroSectionText pageDir={pageDir} />
 
         <HeroImageContainer
           variants={heroImageVariants}
--- styles/themes/default-theme.ts
+++ styles/themes/default-theme.ts
@@ -3,11 +3,13 @@ import { DefaultTheme } from 'styled-components';
 export const defaultTheme: DefaultTheme = {
   colors: {
     primary: '#FFE000',
+    primaryLight: '#7BA3E6',
     secondary: '#ffdada',
     textPrimary: '#CCD6F6',
     textSecondary: '#C2C9DD',
     dark: '#252525',
     blue: '#9747FF',
+    blueLight: '#0077fe',
     white: '#f5f5f5',
     darkWhite: '#dbd8d8',
     buttonDisabled: 'rgba(0,0,0,.8)',
